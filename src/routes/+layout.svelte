a89360529b6b1f6eaff73423e86e0c5e65d60449
Thiago Magano <thiagomagano1993@outlook.com>
2023-07-11 16:30:56 -0300

    wip: initial config

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+// This contains the bulk of Skeletons required styles:
+  import '@skeletonlabs/skeleton/styles/skeleton.css';
+  
+  import '../app.postcss';
+
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+  <title>Thiago Magano - Portfolio - Home</title>
+</svelte:head>
+
 <h1>Welcome to Thiago Magano's Portfolio</h1>
 
 <p>🧑‍🏭 Work in progress.... 🧑‍🏭 </p>
