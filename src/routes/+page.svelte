d00974688a0ef422db14b27f06a0993713404831
Thiago Magano <thiagomagano1993@outlook.com>
2023-07-11 15:44:59 -0300

    Boilerplate Remove

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Thiago Magano's Portfolio</h1>
+
+<p>🧑‍🏭 Work in progress.... 🧑‍🏭 </p>
+
