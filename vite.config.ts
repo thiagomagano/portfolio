395f46d023dfdfce789cf61e162b14640a13e4db
Thiago Magano <thiagomagano1993@outlook.com>
2024-04-11 17:32:17 -0300

    exclude: purgecss dev dependencies bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		purgeCss({
-			safelist: {
-				// any selectors that begin with "hljs-" will not be purged
-				greedy: [/^hljs-/]
-			}
-		})
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
